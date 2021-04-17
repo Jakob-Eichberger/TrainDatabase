dd9d6b105121186f2964c6911965f45f11e6c1b9
Jakob <jakob.eichberger@gmx.net>
2021-04-17 22:05:05 +0200

    Added gitignore and string extensions

--- Extensions/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Extensions
-{
-    public class Class1
-    {
-    }
-}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string e) => string.IsNullOrWhiteSpace(e);
+    }
+}
