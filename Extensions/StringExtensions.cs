3c5cecd6ac7a5898d2bb1fe1a038878590e3490e
Jakob <jakob.eichberger@gmx.net>
2021-04-17 22:10:14 +0200

    trying to fix a bug with gitignore

--- Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Extensions
-{
-    public static class StringExtensions
-    {
-        public static bool IsNullOrWhiteSpace(this string e) => string.IsNullOrWhiteSpace(e);
-    }
-}
--- resetGit.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001
-echo Achtung: Alle lokalen Änderungen werden zurückgesetzt. Drücke CTRL+C zum Abbrechen.
-pause
-git add -A
-git fetch --all
-git reset --hard origin/master
