5106e4cb2fcc26e3ab03a46f7d4317b48a2659ae
Jakob <jakob.eichberger@gmx.net>
2021-04-17 21:16:59 +0200

    added the code for the previous commit.

---
+++ Extensions/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Extensions
+{
+    public class Class1
+    {
+    }
+}
---
+++ WPF/Z21/LokInfoData.cs
@@ -0,0 +1,24 @@
+﻿/* Z21 - C#-Implementierung des Protokolls der Kommunikation mit der digitalen
+ * Steuerzentrale Z21 oder z21 von Fleischmann/Roco
+ * ---------------------------------------------------------------------------
+ * Datei:     z21.cs
+ * Version:   16.06.2014
+ * Besitzer:  Mathias Rentsch (rentsch@online.de)
+ * Lizenz:    GPL
+ *
+ * Die Anwendung und die Quelltextdateien sind freie Software und stehen unter der
+ * GNU General Public License. Der Originaltext dieser Lizenz kann eingesehen werden
+ * unter http://www.gnu.org/licenses/gpl.html.
+ * 
+ */
+
+namespace WpfApplication.Z21
+{
+    public class LokInfoData
+    {
+        public LokAdresse Adresse;
+        public bool Besetzt;
+        public RichtungsAngabe Richtung;
+        public byte Fahrstufe;
+    }
+}
