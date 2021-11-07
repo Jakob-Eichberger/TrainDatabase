c716ff45260e56462a59ddda1026969b53132e20
Jakob <jakob.eichberger@gmx.net>
2021-11-07 21:46:02 +0100

    Update GetLocoFunctionInfoEventArgs.cs

--- WPF/CentralStation/Events/GetLocoFunctionInfoEventArgs.cs
+++ WPF/CentralStation/Events/GetLocoFunctionInfoEventArgs.cs
@@ -5,10 +5,11 @@ namespace WPF_Application.CentralStation.Events
 {
     public class GetLocoFunctionInfoEventArgs : EventArgs
     {
+        public LokInfoFunctionData Data;
+
         public GetLocoFunctionInfoEventArgs(LokInfoFunctionData data) : base()
         {
             Data = data;
         }
-        public LokInfoFunctionData Data;
     }
 }
