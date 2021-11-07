10abfecb001fbd20c11330953b779a34053da118
Jakob <jakob.eichberger@gmx.net>
2021-11-07 21:34:29 +0100

    Revert "Update GetLocoFunctionInfoEventArgs.cs"
    This reverts commit 220036d295b14f7704b17748d935d2bf669eb910.

--- WPF/CentralStation/Events/GetLocoFunctionInfoEventArgs.cs
+++ WPF/CentralStation/Events/GetLocoFunctionInfoEventArgs.cs
@@ -5,11 +5,10 @@ namespace WPF_Application.CentralStation.Events
 {
     public class GetLocoFunctionInfoEventArgs : EventArgs
     {
-        public LokInfoFunctionData Data;
-
         public GetLocoFunctionInfoEventArgs(LokInfoFunctionData data) : base()
         {
             Data = data;
         }
+        public LokInfoFunctionData Data;
     }
 }
