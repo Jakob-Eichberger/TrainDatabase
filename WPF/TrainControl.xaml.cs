2999fbabb33b31a6ca2deeac1b867fcdfde70148
Jakob <jakob.eichberger@gmx.net>
2021-12-04 14:46:50 +0100

    Update TrainControl.xaml.cs

--- WPF/TrainControl.xaml.cs
+++ WPF/TrainControl.xaml.cs
@@ -58,6 +58,8 @@ namespace TrainDatabase
                 controller.OnGetLocoInfo += Controller_OnGetLocoInfo;
                 controller.TrackPowerChanged += Controller_TrackPowerChanged;
                 controller.OnStatusChanged += Controller_OnStatusChanged;
+                controller.GetLocoInfo(new LokAdresse(Vehicle.Address));
+                controller.GetStatus();
 
                 DrawAllFunctions();
                 DrawAllVehicles(db.Vehicles.ToList().Where(m => m.Id != Vehicle.Id));
@@ -85,48 +87,130 @@ namespace TrainDatabase
             Joystick?.Dispose();
         }
 
+        public void RefreshSource()
+        {
+            Vehicle = db.Vehicles.Include(e => e.Functions).ToList().FirstOrDefault(e => e.Id == Vehicle.Id)!;
+            DrawAllFunctions();
+            DrawAllVehicles(db.Vehicles.ToList().Where(m => m.Id != Vehicle.Id));
+        }
+
+        protected void OnPropertyChanged(string propertyName = null!) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Returns a new instance of <typeparamref name="T"/>, which is configured to controll a <see cref="Function"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private static T GetButton<T>(Function item) where T : ButtonBase, new() => new T()
+        {
+            Height = 50,
+            Width = 90,
+            Content = $"{(item.ShowFunctionNumber ? $"({item.FunctionIndex}) " : "")}{item.Name}",
+            Tag = item,
+            ToolTip = $"{item}",
+            Margin = new(10),
+            Padding = new(2),
+            BorderBrush = Brushes.Black,
+            BorderThickness = new(1)
+        };
+
+        private static LokInfoData GetLocoInfoData(int speedstep, bool direction, bool inUse, Vehicle Vehicle) => new()
+        {
+            Adresse = new(Vehicle.Address),
+            DrivingDirection = direction,
+            InUse = inUse,
+            Speed = (byte)speedstep
+        };
+
+        private static double GetSlowestVehicleSpeed(int speedstep, bool direction, (Vehicle Vehicle, (SortedSet<FunctionPoint>? Forwards, SortedSet<FunctionPoint>? Backwards) Traction) Vehicle)
+        {
+            if (Vehicle.Traction.Forwards is null || Vehicle.Traction.Backwards is null)
+                return double.NaN;
+
+            if (!Vehicle.Vehicle.InvertTraction)
+                return direction ? Vehicle.Traction.Forwards.GetYValue(speedstep) : Vehicle.Traction.Backwards.GetYValue(speedstep);
+            else
+                return direction ? Vehicle.Traction.Backwards.GetYValue(speedstep) : Vehicle.Traction.Forwards.GetYValue(speedstep);
+        }
+
+        /// <summary>
+        /// Converts the paramter <paramref name="tractionArray"/> to a <see cref="LineSeries"/> object.
+        /// </summary>
+        /// <param name="tractionArray"></param>
+        /// <returns></returns>
+        private static SortedSet<FunctionPoint>? GetSortedSet(decimal?[] tractionArray)
+        {
+            if (tractionArray[MaxDccSpeed] is null)
+                return null!;
+
+            SortedSet<FunctionPoint>? function = new();
+
+            for (int i = 0; i <= Z21Client.Z21Client.maxDccStep; i++)
+                if (tractionArray[i] is not null)
+                    function.Add(new(i, (double)(tractionArray[i] ?? 0)));
+            return function;
+        }
+
+        private static bool IsVehicleMeasured((Vehicle Vehicle, (SortedSet<FunctionPoint> Forwards, SortedSet<FunctionPoint> Backwards) Traction) item) => item.Traction.Forwards is not null && item.Traction.Backwards is not null;
+
+        private void BtnDirection_Click(object sender, RoutedEventArgs e) => SwitchDirection();
+
+        private async void Controller_OnGetLocoInfo(object? sender, GetLocoInfoEventArgs e)
+        {
+            if (e.Data.Adresse.Value == Vehicle.Address)
+            {
+                LiveData = e.Data;
+                foreach (var (functionIndex, state) in e.Data.Functions)
+                {
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        var x = FunctionToggleButtons.FirstOrDefault(e => (e?.Tag as Function)?.FunctionIndex == functionIndex);
+                        if (x is not null)
+                            x.IsChecked = state;
+                    }));
+                }
+            }
+        }
+
+        private void Controller_OnStatusChanged(object? sender, StateEventArgs e) => TrackPower = e.TrackPower;
+
+        private void Controller_TrackPowerChanged(object? sender, TrackPowerEventArgs e) => TrackPower = e.TrackPower;
+
+        private async Task DeterminSlowestVehicleInList() => await Task.Run(() =>
+                {
+                    var list = DoubleTractionVehicles.Where(e => e.Vehicle.Type == VehicleType.Lokomotive && e.Traction.Forwards is not null && e.Traction.Backwards is not null).ToList();
+
+                    if (list.Any())
+                        SlowestVehicleInTractionList = list.Aggregate((cur, next) => (cur.Traction.Forwards?.GetYValue(MaxDccSpeed) ?? int.MaxValue) < (next.Traction.Forwards?.GetYValue(MaxDccSpeed) ?? int.MaxValue) ? cur : next).Vehicle ?? Vehicle;
+                    else
+                        SlowestVehicleInTractionList = Vehicle;
+                });
+
         /// <summary>
         /// Functions draws every single Function of a vehicle for the user to click on. 
         /// </summary>
-        public void DrawAllFunctions()
+        private void DrawAllFunctions()
         {
             FunctionGrid.Children.Clear();
             foreach (var item in Vehicle.Functions.OrderBy(e => e.FunctionIndex))
             {
-                Border border = new();
-                border.Padding = new(2);
-                border.Margin = new(10);
-                border.BorderThickness = new(1);
-                border.BorderBrush = Brushes.Black;
-
-                StackPanel sp = new();
-
-                if (item.ButtonType == 0)
+                switch (item.ButtonType)
                 {
-                    ToggleButton tb = new() { Height = 50, Width = 90, Content = $"{(item.ShowFunctionNumber ? $"({item.FunctionIndex}) " : "")}{item.Name}" };
-                    tb.Click += FunctionToggle_Click;
-                    tb.Tag = item;
-                    tb.Name = $"btn_Function_{item.Id}";
-                    tb.ToolTip = $"{item}";
-                    FunctionToggleButtons.Add(tb);
-                    sp.Children.Add(tb);
+                    case ButtonType.Switch:
+                        ToggleButton tb = GetButton<ToggleButton>(item);
+                        tb.Click += (sender, e) => SetLocoFunction(((sender as ToggleButton)!.IsChecked ?? false) ? ToggleType.On : ToggleType.Off, ((e.Source as ToggleButton)!.Tag as Function)!);
+                        FunctionToggleButtons.Add(tb);
+                        FunctionGrid.Children.Add(tb);
+                        break;
+                    default:
+                        Button btn = GetButton<Button>(item);
+                        btn.PreviewMouseDown += (sender, e) => SetLocoFunction(ToggleType.On, ((e.Source as Button)?.Tag as Function)!);
+                        btn.PreviewMouseUp += (sender, e) => SetLocoFunction(ToggleType.Off, ((e.Source as Button)?.Tag as Function)!); ;
+                        FunctionButtons.Add(btn);
+                        FunctionGrid.Children.Add(btn);
+                        break;
                 }
-                else
-                {
-                    Button btn = new() { Height = 50, Width = 90, Content = $"{(item.ShowFunctionNumber ? $"({item.FunctionIndex}) " : "")}{item.Name}" };
-                    btn.PreviewMouseDown += FunctionButtonDown_Click;
-                    btn.PreviewMouseUp += FunctionButtonUp_Click;
-                    btn.Tag = item;
-                    btn.Name = $"btn_Function_{item.Id}";
-                    btn.ToolTip = $"{item}";
-                    FunctionButtons.Add(btn);
-                    sp.Children.Add(btn);
-                }
-
-                border.Child = sp;
-                sp.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                sp.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                FunctionGrid.Children.Add(border);
             }
         }
 
@@ -134,7 +218,7 @@ namespace TrainDatabase
         /// Draws every vehicle in a list as a checkbox
         /// </summary>
         /// <param name="vehicles"></param>
-        public void DrawAllVehicles(IEnumerable<Vehicle> vehicles)
+        private void DrawAllVehicles(IEnumerable<Vehicle> vehicles)
         {
             SPVehilces.Children.Clear();
             foreach (var vehicle in vehicles.Where(e => e.Id != Vehicle.Id))
@@ -145,13 +229,37 @@ namespace TrainDatabase
                     Tag = vehicle,
                     Margin = new Thickness(5)
                 };
-                c.Unchecked += VehicleCheckBox_Unchecked;
-                c.Checked += VehicleCheckBox_Checked;
+                c.Unchecked += async (sender, e) =>
+                {
+                    DoubleTractionVehicles.RemoveAll(e => e.Vehicle.Id == (((sender as CheckBox)?.Tag as Vehicle)?.Id ?? -1));
+                    await DeterminSlowestVehicleInList();
+                };
+                c.Checked += async (sender, e) =>
+                 {
+                     if (DoubleTractionVehicles.Count >= 140)
+                     {
+                         MessageBox.Show("Mehr als 140 Traktionsfahrzeuge sind nicht möglich!", "Max Limit reached.", MessageBoxButton.OK, MessageBoxImage.Error);
+                         return;
+                     }
+                     if (sender is CheckBox c && c.Tag is Vehicle v)
+                     {
+                         DoubleTractionVehicles.Add((v, (GetSortedSet(v.TractionForward), GetSortedSet(v.TractionBackward))));
+                         await DeterminSlowestVehicleInList();
+                     }
+                 };
                 SPVehilces.Children.Add(c);
             }
         }
 
-        public void OnJoyStickValueUpdate(object? sender, JoyStickUpdateEventArgs e)
+        private bool GetDrivingDirection(Vehicle vehicle, bool direction) => vehicle.Id != Vehicle.Id ? (vehicle.InvertTraction ? !direction : direction) : direction;
+
+        private double GetSlowestVehicleSpeed(bool direction, int xValue)
+        {
+            var (Id, Traction) = DoubleTractionVehicles.FirstOrDefault(e => e.Vehicle == SlowestVehicleInTractionList);
+            return direction ? Traction.Forwards.GetYValue(xValue) : Traction.Forwards.GetYValue(xValue);
+        }
+
+        private void OnJoyStickValueUpdate(object? sender, JoyStickUpdateEventArgs e)
         {
             try
             {
@@ -203,100 +311,6 @@ namespace TrainDatabase
                 Console.WriteLine($"{nameof(OnJoyStickValueUpdate)} failed! {ex}");
             }
         }
-
-        public void RefreshSource()
-        {
-            Vehicle = db.Vehicles.Include(e => e.Functions).ToList().FirstOrDefault(e => e.Id == Vehicle.Id)!;
-            DrawAllFunctions();
-            DrawAllVehicles(db.Vehicles.ToList().Where(m => m.Id != Vehicle.Id));
-        }
-
-        protected void OnPropertyChanged(string propertyName = null!) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        private static LokInfoData GetLocoInfoData(int speedstep, bool direction, bool inUse, Vehicle Vehicle) => new()
-        {
-            Adresse = new(Vehicle.Address),
-            DrivingDirection = direction,
-            InUse = inUse,
-            Speed = (byte)speedstep
-        };
-
-        private static double GetSlowestVehicleSpeed(int speedstep, bool direction, (Vehicle Vehicle, (SortedSet<FunctionPoint>? Forwards, SortedSet<FunctionPoint>? Backwards) Traction) Vehicle)
-        {
-            if (Vehicle.Traction.Forwards is null || Vehicle.Traction.Backwards is null)
-                return double.NaN;
-
-            if (!Vehicle.Vehicle.InvertTraction)
-                return direction ? Vehicle.Traction.Forwards.GetYValue(speedstep) : Vehicle.Traction.Backwards.GetYValue(speedstep);
-            else
-                return direction ? Vehicle.Traction.Backwards.GetYValue(speedstep) : Vehicle.Traction.Forwards.GetYValue(speedstep);
-        }
-
-        /// <summary>
-        /// Converts the paramter <paramref name="tractionArray"/> to a <see cref="LineSeries"/> object.
-        /// </summary>
-        /// <param name="tractionArray"></param>
-        /// <returns></returns>
-        private static SortedSet<FunctionPoint>? GetSortedSet(decimal?[] tractionArray)
-        {
-            if (tractionArray[MaxDccSpeed] is null)
-                return null!;
-
-            SortedSet<FunctionPoint>? function = new();
-
-            for (int i = 0; i <= Z21Client.Z21Client.maxDccStep; i++)
-                if (tractionArray[i] is not null)
-                    function.Add(new(i, (double)(tractionArray[i] ?? 0)));
-            return function;
-        }
-
-        private static bool IsVehicleMeasured((Vehicle Vehicle, (SortedSet<FunctionPoint> Forwards, SortedSet<FunctionPoint> Backwards) Traction) item) => item.Traction.Forwards is not null && item.Traction.Backwards is not null;
-
-        private async void Controller_OnGetLocoInfo(object? sender, GetLocoInfoEventArgs e)
-        {
-            if (e.Data.Adresse.Value == Vehicle.Address)
-            {
-                LiveData = e.Data;
-                foreach (var (functionIndex, state) in e.Data.Functions)
-                {
-                    await Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        var x = FunctionToggleButtons.FirstOrDefault(e => (e?.Tag as Function)?.FunctionIndex == functionIndex);
-                        if (x is not null)
-                            x.IsChecked = state;
-                    }));
-                }
-            }
-        }
-
-        private void Controller_OnStatusChanged(object? sender, StateEventArgs e) => TrackPower = e.TrackPower;
-
-        private void Controller_TrackPowerChanged(object? sender, TrackPowerEventArgs e) => TrackPower = e.TrackPower;
-
-        private async Task DeterminSlowestVehicleInList() => await Task.Run(() =>
-        {
-            var list = DoubleTractionVehicles.Where(e => e.Vehicle.Type == VehicleType.Lokomotive && e.Traction.Forwards is not null && e.Traction.Backwards is not null).ToList();
-
-            if (list.Any())
-                SlowestVehicleInTractionList = list.Aggregate((cur, next) => (cur.Traction.Forwards?.GetYValue(MaxDccSpeed) ?? int.MaxValue) < (next.Traction.Forwards?.GetYValue(MaxDccSpeed) ?? int.MaxValue) ? cur : next).Vehicle ?? Vehicle;
-            else
-                SlowestVehicleInTractionList = Vehicle;
-        });
-
-        private void FunctionButtonDown_Click(object sender, RoutedEventArgs e) => SetLocoFunction(ToggleType.On, ((e.Source as Button)?.Tag as Function)!);
-
-        private void FunctionButtonUp_Click(object sender, RoutedEventArgs e) => SetLocoFunction(ToggleType.Off, ((e.Source as Button)?.Tag as Function)!);
-
-        private void FunctionToggle_Click(object sender, RoutedEventArgs e) => SetLocoFunction(((sender as ToggleButton)!.IsChecked ?? false) ? ToggleType.On : ToggleType.Off, ((e.Source as ToggleButton)!.Tag as Function)!);
-
-        private bool GetDrivingDirection(Vehicle vehicle, bool direction) => vehicle.Id != Vehicle.Id ? (vehicle.InvertTraction ? !direction : direction) : direction;
-
-        private double GetSlowestVehicleSpeed(bool direction, int xValue)
-        {
-            var (Id, Traction) = DoubleTractionVehicles.FirstOrDefault(e => e.Vehicle == SlowestVehicleInTractionList);
-            return direction ? Traction.Forwards.GetYValue(xValue) : Traction.Forwards.GetYValue(xValue);
-        }
-
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(tbSearch.Text))
@@ -367,50 +381,9 @@ namespace TrainDatabase
                 controller.SetLocoFunction(new((int)vehicle.Address), function, type);
         }
 
-        private void Tc_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Space)
-                e.Handled = true;
-        }
+        private void SliderSpeed_PreviewMouseDown(object sender, MouseButtonEventArgs e) => SliderInUser = true;
 
-        /// <summary>
-        /// Method for event when user checks a checkbox.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void VehicleCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            if (((sender as CheckBox)?.Tag ?? null) is null) return;
-            if (DoubleTractionVehicles.Count >= 140)
-            {
-                MessageBox.Show("Mehr als 140 Traktionsfahrzeuge sind nicht möglich!", "Max Limit reached.", MessageBoxButton.OK);
-                return;
-            }
-            var temp = (Vehicle)(sender as CheckBox)!.Tag;
-            if (temp.Type == VehicleType.Lokomotive && (temp.TractionForward[Z21Client.Z21Client.maxDccStep] is null || temp.TractionForward[Z21Client.Z21Client.maxDccStep] is null))
-                MessageBox.Show("Achtung! Fahrzeug ist nicht eingemessen!");
-            DoubleTractionVehicles.Add((temp, (GetSortedSet(temp.TractionForward), GetSortedSet(temp.TractionBackward))));
-            await DeterminSlowestVehicleInList();
-        }
-
-        /// <summary>
-        /// Method for event when user unchecks a checkbox.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void VehicleCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if ((sender as CheckBox)?.Tag is null)
-                return;
-
-            var temp = (Vehicle)(sender as CheckBox)!.Tag;
-
-            DoubleTractionVehicles.RemoveAll(e => e.Vehicle.Id == temp.Id);
-
-            await DeterminSlowestVehicleInList();
-        }
-
-        private void BtnDirection_Click(object sender, RoutedEventArgs e) => SwitchDirection();
+        private void SliderSpeed_PreviewMouseUp(object sender, MouseButtonEventArgs e) => SliderInUser = false;
 
         private void SwitchDirection() => SetLocoDrive(drivingDirection: !LiveData.DrivingDirection);
 
@@ -420,15 +393,17 @@ namespace TrainDatabase
 
         private void Tc_Deactivated(object sender, EventArgs e) => IsActive = false;
 
+        private void Tc_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Space)
+                e.Handled = true;
+        }
+
         private void Tc_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             Speed = e.Delta < 0 ? Speed - 1 : Speed + 1;
             e.Handled = true;
             SliderLastused = DateTime.Now;
         }
-
-        private void SliderSpeed_PreviewMouseDown(object sender, MouseButtonEventArgs e) => SliderInUser = true;
-
-        private void SliderSpeed_PreviewMouseUp(object sender, MouseButtonEventArgs e) => SliderInUser = false;
     }
 }
