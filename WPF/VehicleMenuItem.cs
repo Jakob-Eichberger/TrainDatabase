152b64fe6e30ab3b8d61da1175eb6e2b50e8ab40
Jakob <jakob.eichberger@gmx.net>
2021-12-04 14:46:55 +0100

    Create VehicleMenuItem.cs

---
+++ WPF/VehicleMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using Model;
+using System.Windows.Controls;
+
+namespace Wpf_Application
+{
+    public class VehicleMenuItem : MenuItem
+    {
+        public Vehicle Vehicle { get; set; } = default!;
+    }
+}
