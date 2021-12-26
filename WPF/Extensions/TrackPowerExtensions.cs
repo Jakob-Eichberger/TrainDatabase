58c52e2198f096281fcf0332f96fccb9d558809e
Jakob <jakob.eichberger@gmx.net>
2021-12-26 16:30:23 +0100

    Delete TrackPowerExtensions.cs

--- WPF/Extensions/TrackPowerExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using TrainDatabase.Z21Client.Enum;
-
-namespace TrainDatabase.Extensions
-{
-    public static class TrackPowerExtensions
-    {
-        public static bool ToBoolean(this TrackPower e) => e == TrackPower.ON;
-    }
-}
