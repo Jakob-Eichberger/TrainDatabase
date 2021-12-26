e2d1e90f8182c4715f225f26e90e4d673a8d93cb
Jakob <jakob.eichberger@gmx.net>
2021-12-26 16:28:12 +0100

    Create VehicleTypeExtensions.cs

---
+++ Extensions/VehicleTypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Model;
+
+namespace Extensions
+{
+    public static class VehicleTypeExtensions
+    {
+        public static bool IsLokomotive(this VehicleType e) => e == VehicleType.Lokomotive;
+
+        public static bool IsSteuerwagen(this VehicleType e) => e == VehicleType.Steuerwagen;
+
+        public static bool IsWagen(this VehicleType e) => e == VehicleType.Wagen;
+    }
+}
