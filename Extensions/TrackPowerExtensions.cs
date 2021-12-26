8c08dad0ac4a73f0f04fb2e04db35f70c73f405d
Jakob <jakob.eichberger@gmx.net>
2021-12-26 16:30:21 +0100

    Create TrackPowerExtensions.cs

---
+++ Extensions/TrackPowerExtensions.cs
@@ -0,0 +1,9 @@
+﻿using TrainDatabase.Z21Client.Enum;
+
+namespace Extensions
+{
+    public static class TrackPowerExtensions
+    {
+        public static bool ToBoolean(this TrackPower e) => e == TrackPower.ON;
+    }
+}
