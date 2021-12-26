22eafb768e31ace33c1f84fb173681cd7521fa2a
Jakob <jakob.eichberger@gmx.net>
2021-12-26 16:04:34 +0100

    Update VehicleManagement.xaml.cs

--- WPF/VehicleManagement.xaml.cs
+++ WPF/VehicleManagement.xaml.cs
@@ -15,11 +15,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using TrainDatabase.Extensions;
-using TrainDatabase.Infrastructure;
 using LineSeries = OxyPlot.Series.LineSeries;
 using LinearAxis = OxyPlot.Axes.LinearAxis;
 using Renci.SshNet;
 using System.Collections.ObjectModel;
+using Extensions;
+using Infrastructure;
 
 namespace TrainDatabase
 {
