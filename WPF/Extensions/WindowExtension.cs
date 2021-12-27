087a2dd649812eb27acd345a5cf5ac3b4a301f25
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2021-12-27 17:04:14 +0100

    Create WindowExtension.cs

---
+++ WPF/Extensions/WindowExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Application.Extensions
+{
+    static internal class WindowExtension
+    {
+        internal static void OpenOneInstance<T>(this T window) where T : Window, new()
+        {
+            if (Application.Current.Windows.OfType<T>().FirstOrDefault() is T settings)
+            {
+                settings.WindowState = WindowState.Normal;
+                settings.Activate();
+            }
+            else
+                ((T)Activator.CreateInstance(typeof(T))).Show();
+        }
+    }
+}
