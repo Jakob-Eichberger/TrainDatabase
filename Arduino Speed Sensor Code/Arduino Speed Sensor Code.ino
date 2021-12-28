a4961584e47e47f89e8d22412d09c40d5bb06ae0
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2021-12-28 01:13:18 +0100

    Switched from a Pi to an Arduino.

---
+++ Arduino Speed Sensor Code/Arduino Speed Sensor Code.ino
@@ -0,0 +1,41 @@
+int const pinLeft = A1;
+int const pinRight = A2;
+double const val = 1000000.0;
+int const waitCounter = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinLeft, INPUT);
+  pinMode(pinRight, INPUT);
+}
+
+void loop() {
+  if (pinOn(pinRight)) {
+    getTime(pinLeft);
+  } else if (pinOn(pinLeft)) {
+    getTime(pinRight);
+  }
+}
+
+bool pinOn(int pin) {
+  return analogRead(pin) < 500;
+}
+
+void getTime(int pin2) {
+  long start = micros();
+  while (!pinOn(pin2)) {
+    continue;
+  }
+  long end = micros();
+  WaitForSensorClear(pin2);
+  Serial.println((end - start) / val, 22);
+}
+
+void WaitForSensorClear(int pin2) {
+  int counter = 0;
+  while (counter <= waitCounter) {
+    delay(100);
+    if (!pinOn(pin2))
+      counter++;
+  }
+}
