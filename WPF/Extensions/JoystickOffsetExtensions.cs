059889644e8d00af29a6978dcfe696a0af463460
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-03-20 22:56:23 +0100

    Update JoystickOffsetExtensions.cs

--- WPF/Extensions/JoystickOffsetExtensions.cs
+++ WPF/Extensions/JoystickOffsetExtensions.cs
@@ -11,14 +11,14 @@ namespace TrainDatabase.Extensions
         /// </summary>
         /// <param name="e"></param>
         /// <returns></returns>
-        public static int GetMaxValue(this JoystickOffset e) => Convert.ToInt32(Settings.Get($"{Enum.GetName(e)}_MaxValue")!.IsNullOrWhiteSpace(out string v) ? "0" : v);
+        public static int GetMaxValue(this JoystickOffset e) => Convert.ToInt32(Configuration.Get($"{Enum.GetName(e)}_MaxValue")!.IsNullOrWhiteSpace(out string v) ? "0" : v);
 
         /// <summary>
         /// Sets the max value when a JoyStick Button is Pressed on a Joystick. Stores the Data in App.Config.
         /// </summary>
         /// <param name="e"></param>
         /// <param name="value"></param>
-        public static void SetMaxValue(this JoystickOffset e, int value) => Settings.Set($"{Enum.GetName(e)}_MaxValue", value.ToString());
+        public static void SetMaxValue(this JoystickOffset e, int value) => Configuration.Set($"{Enum.GetName(e)}_MaxValue", value.ToString());
 
     }
 }
