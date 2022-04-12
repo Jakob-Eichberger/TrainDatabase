7b1e127d3fe05eaf34b40fa0f4b48f09295d9c4e
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-12 23:28:44 +0200

    Update VehicleMenuItem.cs

--- WPF/VehicleMenuItem.cs
+++ WPF/VehicleMenuItem.cs
@@ -1,10 +1,18 @@
 ﻿using Model;
+using System;
 using System.Windows.Controls;
 
 namespace Wpf_Application
 {
     public class VehicleMenuItem : MenuItem
     {
+        public VehicleMenuItem(Vehicle vehicle, string content, Action<Vehicle> onClick)
+        {
+            Vehicle = vehicle;
+            Header = content;
+            Click += (a, b) => onClick(Vehicle);
+        }
+
         public Vehicle Vehicle { get; set; } = default!;
     }
 }
