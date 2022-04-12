711153f3f8eb255f2ab07b34d6abe94c05f584e9
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-12 23:28:59 +0200

    Create MessageLoggedEventArgs.cs

---
+++ Helper/MessageLoggedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Helper
+{
+    public class MessageLoggedEventArgs : EventArgs
+    {
+        public MessageLoggedEventArgs(string message, MessageTypeEnum type, object value = null) : base()
+        {
+            Message = message;
+            Type = type;
+            Value = value;
+        }
+
+        public MessageLoggedEventArgs(MessageTypeEnum type, object value = null) : base()
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public DateTime DateTime { get; } = DateTime.Now;
+
+        public string? Message { get; }
+
+        public MessageTypeEnum Type { get; }
+
+        public object? Value { get; }
+    }
+}
