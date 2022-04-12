5044d988a81e0ae98622dd2dc0002443faad049c
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-12 23:29:00 +0200

    Update Logger.cs

--- Helper/Logger.cs
+++ Helper/Logger.cs
@@ -6,13 +6,25 @@ namespace Helper
 {
     public class Logger
     {
-        public static void Log(string message = "", Exception exception = null!, [CallerMemberName] string functionName = default!)
+        public static event EventHandler<MessageLoggedEventArgs>? OnMessageLogged = default!;
+
+        public static void LogError(Exception exception, string message)
         {
-            Console.WriteLine(message);
+            OnMessageLogged?.Invoke(null, new MessageLoggedEventArgs($"{message}", MessageTypeEnum.Error, exception));
+            LogToFile(message, exception);
+        }
+
+        public static void LogInformation(string message, object value = null) => OnMessageLogged?.Invoke(null, new MessageLoggedEventArgs(message, MessageTypeEnum.Information, value));
+
+        public static void LogWarning(string message, object value = null) => OnMessageLogged?.Invoke(null, new MessageLoggedEventArgs(message, MessageTypeEnum.Warning, value));
+
+        private static void LogToFile(string message, Exception exception)
+        {
+            Logger.LogInformation(message);
             string path = Directory.GetCurrentDirectory() + @"\Log";
             Directory.CreateDirectory(path);
             path += $@"\{DateTime.UtcNow.Date:dd-MM-yyyy}.txt";
-            File.AppendAllText(path, $"ERROR in function '{functionName ?? " - "}' ({DateTime.UtcNow:dd.MM.yyyy HH:mm:ss}): '{message}'\n\n Exception: {exception} \n\n------------------------------------------------------------------------------------------------\n");
+            File.AppendAllText(path, $"({DateTime.UtcNow:dd.MM.yyyy HH:mm:ss}): '{message}'\n\n Exception: {exception} \n\n------------------------------------------------------------------------------------------------\n");
         }
     }
 }
