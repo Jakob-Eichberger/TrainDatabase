6d973ec576932aa7987a652aa780d25907179d40
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-17 00:40:48 +0200

    Create VehicleService.cs

---
+++ Service/VehicleService.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure;
+using Model;
+using System;
+
+namespace Service
+{
+    public class VehicleService
+    {
+        public VehicleService(Database db)
+        {
+            Db = db;
+        }
+        public Database Db { get; set; }
+        public void AddTractionVehilce(Vehicle vehicle1, Vehicle vehicle2)
+        {
+            vehicle1.TractionVehicleIds.Add(vehicle2.Id);
+            vehicle2.TractionVehicleIds.Add(vehicle1.Id);
+            Db.Update(vehicle1);
+            Db.Update(vehicle2);
+        }
+
+        public void RemoveTractionVehilce(Vehicle vehicle1, Vehicle vehicle2)
+        {
+            vehicle1.TractionVehicleIds.Remove(vehicle2.Id);
+            vehicle2.TractionVehicleIds.Remove(vehicle1.Id);
+            Db.Update(vehicle1);
+            Db.Update(vehicle2);
+        }
+    }
+}
