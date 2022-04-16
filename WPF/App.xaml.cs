bd1f58d11118f9e2388467e0187c56f188ab264b
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-16 14:39:42 +0200

    Update App.xaml.cs

--- WPF/App.xaml.cs
+++ WPF/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+using TrainDatabase;
+using TrainDatabase.Z21Client;
+using WPF_Application;
 
 namespace Wpf_Application
 {
@@ -7,5 +13,37 @@ namespace Wpf_Application
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider serviceProvider { get; }
+
+        public App()
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddDbContext<Database>();
+            services.AddSingleton<Z21Client>();
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<LogWindow>();
+            //services.AddSingleton<ServiceProvider>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                if (Configuration.OpenDebugConsoleOnStart)
+                    serviceProvider.GetService<LogWindow>()!.Show();
+
+                serviceProvider.GetService<MainWindow>()!.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Application failed to start. {ex.Message}");
+            }
+        }
     }
 }
