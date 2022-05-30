67b9079b821f8251ef8511a7936b649ee1dbf84b
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-05-30 23:41:01 +0200

    Create PushButton.cs

---
+++ WPF/TrainControl/FunctionButton/PushButton.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using Viewmodel;
+
+namespace WPF_Application.TrainControl.FunctionButton
+{
+    internal class PushButton : FunctionBase
+    {
+        public PushButton(IServiceProvider serviceProvider, FunctionModel functionModel) : base(serviceProvider, functionModel)
+        {
+            PreviewMouseDown += (sender, e) => Function.SetState(true);
+            PreviewMouseUp += (sender, e) => Function.SetState(false);
+        }
+    }
+}
