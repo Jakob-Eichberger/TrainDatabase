2b5e8079ed00f3faa8672c285a0be1e3209ff7b4
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-05-30 23:41:05 +0200

    Create FunctionBase.cs

---
+++ WPF/TrainControl/FunctionButton/FunctionBase.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Windows.Controls;
+using Viewmodel;
+
+namespace WPF_Application.TrainControl.FunctionButton
+{
+    internal abstract class FunctionBase : Button
+    {
+        public FunctionBase(IServiceProvider serviceProvider, FunctionModel functionModel)
+        {
+            ServiceProvider = serviceProvider;
+            FunctionModel = functionModel;
+            FunctionButton.ApplyStyle(this, FunctionModel);
+
+            Function = new Function(ServiceProvider, functionModel);
+        }
+
+        private IServiceProvider ServiceProvider { get; }
+
+        private FunctionModel FunctionModel { get; }
+
+        internal Function Function { get; }
+    }
+}
