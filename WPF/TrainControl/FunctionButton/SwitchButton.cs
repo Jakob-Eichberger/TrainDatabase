3c426353d7ccaf305f02bbc67b73e7c32e01c54e
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-05-30 23:40:59 +0200

    Create SwitchButton.cs

---
+++ WPF/TrainControl/FunctionButton/SwitchButton.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls.Primitives;
+using Viewmodel;
+
+namespace WPF_Application.TrainControl.FunctionButton
+{
+    internal class SwitchButton : ToggleButton
+    {
+        public SwitchButton(IServiceProvider serviceProvider, FunctionModel functionModel)
+        {
+            if (functionModel.ButtonType is not ButtonType.Switch)
+                throw new ApplicationException($"Button is type {functionModel.ButtonType} but should be {ButtonType.Switch}");
+
+            ServiceProvider = serviceProvider;
+            FunctionModel = functionModel;
+            FunctionButton.ApplyStyle(this, FunctionModel);
+
+            Function = new Function(ServiceProvider, functionModel);
+            Function.StateChanged += (a, state) => Dispatcher.Invoke(() => IsChecked = state);
+            Click += (a, b) => Function.SetState(IsChecked ?? false);
+        }
+
+        private IServiceProvider ServiceProvider { get; }
+
+        private FunctionModel FunctionModel { get; }
+
+        private Function Function { get; }
+    }
+}
