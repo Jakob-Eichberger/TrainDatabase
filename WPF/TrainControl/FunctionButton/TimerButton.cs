55baa586bebc0671a147966eba6d34684c8d81fd
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-05-30 23:40:55 +0200

    Create TimerButton.cs

---
+++ WPF/TrainControl/FunctionButton/TimerButton.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using System;
+using System.Threading.Tasks;
+using Viewmodel;
+
+namespace WPF_Application.TrainControl.FunctionButton
+{
+    internal class TimerButton : FunctionBase
+    {
+        public TimerButton(IServiceProvider serviceProvider, FunctionModel functionModel) : base(serviceProvider, functionModel)
+        {
+            PreviewMouseDown += async (sender, e) =>
+            {
+                Function.SetState(true);
+                await Task.Delay(new TimeSpan(0, 0, functionModel.Time));
+                Function.SetState(false);
+            };
+            Function.StateChanged += (a, state) => Dispatcher.Invoke(() => IsEnabled = !state);
+        }
+    }
+}
