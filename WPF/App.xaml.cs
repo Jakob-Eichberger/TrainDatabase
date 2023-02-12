303b5437a102bfe67d340ba5a3c02e30fd3233bb
Jakob <jakob.eichberger@gmx.net>
2023-02-12 17:28:19 +0100

    Add AppDomain.CurrentDomain.UnhandledException handl

--- WPF/App.xaml.cs
+++ WPF/App.xaml.cs
@@ -23,6 +23,8 @@ namespace Wpf_Application
 
         public App()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             ServiceCollection services = new();
             ConfigureServices(services);
             Log.Logger = new LoggerConfiguration()
@@ -34,6 +36,12 @@ namespace Wpf_Application
                         .CreateLogger();
 
             ServiceProvider = services.BuildServiceProvider();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            Log.Logger.Error($"{ex?.Message}");
         }
 
         private static void ConfigureServices(ServiceCollection services)
