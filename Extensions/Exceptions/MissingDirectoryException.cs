91ff9fb91c7481e03ae1489e6467d1b1ba04c822
Jakob <jakob.eichberger@gmx.net>
2023-02-19 17:42:11 +0100

    Add MissingDirectoryException to helper

---
+++ Extensions/Exceptions/MissingDirectoryException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when a directory, that is expected to be here, is not found.
+    /// </summary>
+    public class MissingDirectoryException : Exception
+    {
+        /// <summary>
+        /// Exception that is thrown when a directory, that is expected to be here, is not found.
+        /// </summary>
+        public MissingDirectoryException(DirectoryInfo directoryInfo) : base(message: $"Expected to find a directory at the following location: {directoryInfo}")
+        {
+        }
+
+        /// <summary>
+        /// Exception that is thrown when a directory, that is expected to be here, is not found.
+        /// </summary>
+        public MissingDirectoryException(string directoryInfo) : base(message: $"Expected to find a directory at the following location: {directoryInfo}")
+        {
+        }
+    }
+}
